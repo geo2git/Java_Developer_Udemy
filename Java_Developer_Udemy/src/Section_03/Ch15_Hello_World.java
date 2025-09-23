e77321d17e2fcf09f4b5857d62e37da46923a312
emigeo <george2florescu@yahoo.com>
2025-09-23 13:25:58 +0300

    java developer first step

---
+++ Java_Developer_Udemy/src/Section_03/Ch15_Hello_World.java
@@ -0,0 +1,9 @@
+package Section_03;
+
+public class Ch15_Hello_World {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World !!");
+	}
+}
---
+++ Java_Developer_Udemy/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Java_Developer_Udemy {
+}
