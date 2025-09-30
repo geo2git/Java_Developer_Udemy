abb8ecf2fd0932efbdd1d1d7a8ba58e00475c3b8
George <george2florescu@yahoo.com>
2025-09-30 19:29:24 +0300

    ch15

--- Java_Developer_Udemy/src/Section_03/Ch15_Hello_World.java
+++ Java_Developer_Udemy/src/Section_03/Ch15_Hello_World.java
@@ -5,5 +5,10 @@ public class Ch15_Hello_World {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World !!");
+		
+		String numberAsString = "123";
+        int number = Integer.parseInt(numberAsString);
+
+        System.out.println("Numărul este: " + number);
 	}
 }
