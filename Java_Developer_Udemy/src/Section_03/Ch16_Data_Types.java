059ec260659c0cdac3282246dc0af2fb9f330f3b
George <george2florescu@yahoo.com>
2025-09-30 19:16:39 +0300

    ch16

---
+++ Java_Developer_Udemy/src/Section_03/Ch16_Data_Types.java
@@ -0,0 +1,13 @@
+package Section_03;
+
+public class Ch16_Data_Types {
+
+	public static void main(String[] args) {
+		
+		int nrMeu = 25;
+		int a = 23; int g = 2; int sum=a+g;
+		
+		System.out.println("Nr meu este: " + nrMeu);
+		System.out.println("Suma celor 2 nr este: " + sum);
+	}
+}
