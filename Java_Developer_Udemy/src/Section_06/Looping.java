dc2a7c5716c49681bf65fdb4a927352dd3620fb2
George <george2florescu@yahoo.com>
2025-12-15 18:06:07 +0200

    we

---
+++ Java_Developer_Udemy/src/Section_06/Looping.java
@@ -0,0 +1,30 @@
+package Section_06;
+
+public class Looping {
+	
+	public static void main(String[] args) {
+		
+		for (int counter=1; counter<=5; counter ++) {
+			System.out.println(counter);
+		}
+		
+	
+		for (double rata=2; rata<5; rata++) {
+			double dobanda=calculeazaDobanda(10000, rata);
+		
+		System.out.println("10000 de Euro la dobanda de "  + rata +  "% inseamna = " + dobanda + " de Euro");
+		}
+		
+		for (double rataB=7.5; rataB<=10; rataB+=0.25) {
+			double dobandaB=calculeazaDobanda(10000,rataB);
+			System.out.println("10000 de Euro la dobanda de "  + rataB +  "% inseamna = " + dobandaB + " de Euro");
+		}
+
+	    }
+		public static double calculeazaDobanda(double suma, double rataDobanzii) {
+			
+			return (suma*(rataDobanzii/100));			
+		}
+		
+
+}
---
+++ Java_Developer_Udemy/src/Section_06/PrimeNumber.java
@@ -0,0 +1,49 @@
+package Section_06;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		
+		System.out.println("0 este " + (esteNrPrim(0) ? "" : "NU") + " un nr prim");
+		System.out.println("1 este " + (esteNrPrim(1) ? "" : "NU") + " un nr prim");
+		System.out.println("2 este " + (esteNrPrim(2) ? "" : "NU") + " un nr prim");
+		System.out.println("17 este " + (esteNrPrim(17) ? "" : "NU") + " un nr prim");
+		System.out.println("31 este " + (esteNrPrim(31) ? "" : "NU") + " un nr prim");
+	
+		
+		int count =0;
+		
+		for(int i=0; i<=1000;i++) {
+			//System.out.println("Nr: " + count + (esteNrPrim(count) ? "" : " NU") + " un nr prim\"");
+			
+			if(esteNrPrim(i)) {
+				System.out.println("Nr: " + i + " este un nr prim");
+				count ++;
+				if(count==3) {
+					break;
+				}
+			}
+			
+		}
+		
+		System.out.println("Nr total de nr prime gasite este: " + count);
+	 }
+	
+		
+	public static boolean esteNrPrim(int nrIntreg) {
+		
+		if(nrIntreg<=2) {
+			return (nrIntreg==2);
+		}
+		
+		for (int div=2; div<=nrIntreg/2; div++) {
+			if(nrIntreg%div==0) {
+				return false;
+			}
+		}
+		return true;
+				
+	}
+	
+	
+}
