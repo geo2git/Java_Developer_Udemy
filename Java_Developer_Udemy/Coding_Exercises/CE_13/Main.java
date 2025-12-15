5993f6269201a848efe14188312fdbc93119e266
George <george2florescu@yahoo.com>
2025-12-15 13:00:19 +0200

    ww

---
+++ Java_Developer_Udemy/Coding_Exercises/CE_13/Main.java
@@ -0,0 +1,10 @@
+package CE_13;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	//	int nr = NumberInWord.printNumberInWord(3);
+		NumberInWord.printNumberInWord(3);
+	}
+}
---
+++ Java_Developer_Udemy/Coding_Exercises/CE_14/Main.java
@@ -0,0 +1,10 @@
+package CE_14;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		boolean b = NumberOfDaysInMonth.isLeapYear(2025);
+		System.out.println(b);
+	}
+}
