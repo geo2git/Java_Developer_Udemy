6277249edfaa689b4006154453fe056cff752550
George <george2florescu@yahoo.com>
2025-12-19 16:26:30 +0200

    fwe

--- Java_Developer_Udemy/src/Section_06/Ch75_Scanner_Challenge.java
+++ Java_Developer_Udemy/src/Section_06/Ch75_Scanner_Challenge.java
@@ -1,5 +1,22 @@
 package Section_06;
+import java.util.*;
 
 public class Ch75_Scanner_Challenge {
 
+	public static void main(String[] args) {
+		
+Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Introduceti numele dvs: ");
+		String nume = sc.nextLine();
+		
+		Scanner nr1 =  new sc
+	}
+	
+	public static int getNrIntroduse(int nr1, int nr2, int nr3, int nr4, int nr5) {
+		
+		Scanner sc1 = new Scanner(System.in);
+		
+		System.out.println("Introduceti primul nr: ");
+	}
 }
